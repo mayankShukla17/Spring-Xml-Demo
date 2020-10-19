24db0b846ee7df1ec0954ed2308f538cd4d30090
Mayank Shukla <mayankgonda@gmail.com>
2020-10-19 14:00:14 +0000

    Spring XML Config. Task 1 Completed

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,26 +1,15 @@
 package com.stackroute;
 /*1. Task 1:
-<<<<<<< HEAD
-Create a Maven project and add required dependency of spring-context 5.1.4.RELEASE Create a Main class in package com.stackroute and two Spring Beans – Movie, and Actor in 
-package com.stackroute.domain. Actor has two String properties, name and gender, and an age property of type int. An Actor can be initialized with the three properties via 
-the corresponding setter methods. Use property based injection in the bean definition file (beans.xml) Movie “has a” Actor that can be initialized via the corresponding 
-=======
+
 Create a Maven project and add required dependency of spring-context 5.1.4.RELEASE Create a Main class in package com.stackroute and two Spring Beans – Movie, and Actor 
 in package com.stackroute.domain. Actor has two String properties, name and gender, and an age property of type int. An Actor can be initialized with the three properties 
 via the corresponding setter methods. Use property based injection in the bean definition file (beans.xml) Movie “has a” Actor that can be initialized via the corresponding 
->>>>>>> 679fab2f769c562d592e4d09e8cbc2891c94af7b
 setter method. Use property based object injection in the bean definition file (beans.xml) The Main class looks up Movie bean via three ways to print out actor information:
 
 	1. Using XmlBeanFactory
 	2. Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader
-<<<<<<< HEAD
 	3. Using ApplicationContext Create a spring-xml-demo repo and push the code to master branch.
 */
-=======
-	3. Using ApplicationContext 
-Create a spring-xml-demo repo and push the code to master branch.
- */
->>>>>>> 679fab2f769c562d592e4d09e8cbc2891c94af7b
 
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
@@ -31,7 +20,6 @@ import org.springframework.core.io.ClassPathResource;
 import com.stackroute.domain.Movie;
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
 		// Using XMLBeanFactory
 		XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("beans.xml"));
@@ -48,23 +36,5 @@ public class Main {
 		System.out.println(movie1.getActor());
 
 		System.out.println(movie2.getActor());
-=======
-		
-		// Using XMLBeanFactory
-		XmlBeanFactory xmlBeanFactory= new XmlBeanFactory ( new ClassPathResource("Beans.xml"));
-		Movie movieFirst= (Movie)xmlBeanFactory.getBean("movie1");
-
-		//Using BeanDefinitionRegistry and BeanDefinitionReader
-		//BeanDefinitionRegistry beanDefinitionRegistry= new BeanDefinitionReader("Beans.xml");
-		
-		
-		// Using ApplicationContext
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
-		Movie movie=(Movie)applicationContext.getBean("movie2");
-
-		
-		System.out.println(movieFirst.getActor());
-		System.out.println(movie.getActor());
->>>>>>> 679fab2f769c562d592e4d09e8cbc2891c94af7b
 	}
 }
